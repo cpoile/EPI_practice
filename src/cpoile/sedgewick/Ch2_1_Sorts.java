f49f11d732cc12d29eec7567be07fbe3581e046f
Chris Poile <cpoile@gmail.com>
2018-10-21 19:55:11 -0400

    3way quicksort

--- src/cpoile/sedgewick/Ch2_1_Sorts.java
+++ src/cpoile/sedgewick/Ch2_1_Sorts.java
@@ -25,7 +25,8 @@ class Sort {
         //shellSort(a);
         //mergeSort(a);
         //bottomUpMergeSort(a);
-        quickSort(a);
+        //quickSort(a);
+        quick3WaySort(a);
     }
 
     public static void selectionSort(Comparable[] a) {
@@ -128,27 +129,50 @@ class Sort {
         }
     }
 
+
     private static void quickSort(Comparable[] a) {
         StdRandom.shuffle(a);
         quickSort(a, 0, a.length-1);
     }
+
     private static void quickSort(Comparable[] a, int lo, int hi) {
         if (hi <= lo) return;
         int j = partition(a, lo, hi);
-        quickSort(a, 0, j - 1);
-        quickSort(a, j + 1, hi);
+        quickSort(a, lo, j-1);
+        quickSort(a, j+1, hi);
     }
+
     private static int partition(Comparable[] a, int lo, int hi) {
         Comparable v = a[lo];
-        int i = lo, j = hi+1;
+        int i = lo;
+        int j = hi +1;
         while (true) {
-            while (less(a[++i], v)) if (i == hi) break;
             while (less(v, a[--j])) if (j == lo) break;
-            if (i >= j) break;
+            while (less(a[++i], v)) if (i == hi) break;
+            if (j <= i) break;
             exch(a, i, j);
         }
         exch(a, lo, j);
         return j;
+    }
+
+    private static void quick3WaySort(Comparable[] a) {
+        StdRandom.shuffle(a);
+        quick3WaySort(a, 0, a.length-1);
+    }
+    private static void quick3WaySort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        int lt = lo, gt = hi, i = lo+1;
+        Comparable v = a[lo];
+        while (i <= gt) {
+            int cmp = a[i].compareTo(v);
+            if (cmp < 0) exch(a, lt++, i++);
+            else if (cmp > 0) exch(a, i, gt--);
+            else i++;
+        }
+        //exch(a, lo, lt);
+        quick3WaySort(a, lo, lt-1);
+        quick3WaySort(a, gt+1, hi);
     }
 
     // Supporting functions
@@ -186,6 +210,28 @@ class Sort {
         System.out.println();
     }
 
+    private static void quickSort1(Comparable[] a) {
+        StdRandom.shuffle(a);
+        quickSort(a, 0, a.length-1);
+    }
+    private static void quickSort1(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        int j = partition(a, lo, hi);
+        quickSort(a, 0, j - 1);
+        quickSort(a, j + 1, hi);
+    }
+    private static int partition1(Comparable[] a, int lo, int hi) {
+        Comparable v = a[lo];
+        int i = lo, j = hi+1;
+        while (true) {
+            while (less(a[++i], v)) if (i == hi) break;
+            while (less(v, a[--j])) if (j == lo) break;
+            if (i >= j) break;
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
+    }
     static boolean isSorted(Comparable[] a) {
         for (int i = 0; i < a.length - 1; i++) {
             if (less(a[i + 1], a[i])) return false;
